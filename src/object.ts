4466dbf9c8bedbd588f6a124e5a0c3545cdaf86c
I079914 <rong.wei@sap.com>
2017-12-05 15:55:54 +0800

    'object.ts'

---
+++ src/object.ts
@@ -0,0 +1,32 @@
+export function deepClone(source: any) {
+    let target
+    // Handle 3 simple types
+    if (null == source || 'object' !== typeof source) return source
+
+    // Handle date
+    if (source instanceof Date) {
+        target = new Date()
+        target.setTime(source.getTime())
+        return target
+
+    }
+
+    // Handle array
+    if (source instanceof Array) {
+        target = []
+        for ( let i = 0, len = source.length; i < len; i++ ) {
+            target[i]  = deepClone(source[i])
+        }
+        return target
+    }
+
+  // Handle object
+    if (source instanceof Object) {
+      target = {}
+      for (let attr in source) {
+          if ( source.hasOwnProperty(attr)) target[attr] = deepClone(source[attr])
+      }
+      return target
+  }
+
+}
