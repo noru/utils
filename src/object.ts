2a17fed8c5c38d2de55b3aad61b9d09d3faac6d5
Rong Wei <rong.w1992@gmail.com>
2017-12-06 23:31:29 +0800

    isequalmethod

--- src/object.ts
+++ src/object.ts
@@ -1,38 +1,69 @@
 /**
+ * Is this object a simple type? null, undefine, string, num...
+ *
+ * @export
+ * @param {*} obj
+ * @returns {boolean}
+ */
+export function isSimpleType(obj: any): boolean {
+  return null == obj || 'object' !== typeof obj
+}
+/**
  * Deep clone one object to another
  * @export
  * @param {*} source any
  * @returns any
  */
 export function deepClone(source: any) {
-    let target
-    // Handle 3 simple types
-    if (null == source || 'object' !== typeof source ) return source
+  let target
+    // Handle simple types
+  if (isSimpleType(source)) return source
 
-    // Handle date ->not needed its an object
-    if (source instanceof Date) {
-        target = new Date()
-        target.setTime(source.getTime())
-        return target
-    }
-
+    // Handle date
+  if (source instanceof Date) {
+      target = new Date()
+      target.setTime(source.getTime())
+      return target
+  }
     // Handle array
-    if (source instanceof Array) {
-        target = []
-        for ( let i = 0, len = source.length; i < len; i++ ) {
-            target[i]  = deepClone(source[i])
+  if (source instanceof Array) {
+      target = []
+      for ( let i = 0, len = source.length; i < len; i++ ) {
+          target[i]  = deepClone(source[i])
         }
-        return target
-    }
-
+      return target
+  }
   // Handle object
-    if (source instanceof Object) {
+  if (source instanceof Object) {
       target = {}
       for (let attr in source) {
-          if ( !source.hasOwnProperty(attr))  throw new Error( 'Object type is not supported. Clone failed')
-          target[attr] = deepClone(source[attr])
+          if (source.hasOwnProperty(attr)) target[attr] = deepClone(source[attr])
       }
       return target
   }
-   // throw new TypeError( 'Object type is not supported. Clone failed')
+}
+/**
+ * Is object A equals to object B?
+ *
+ * @export
+ * @param {*} obj1
+ * @param {*} obj2
+ * @returns
+ */
+export function isObjectEqual(obj1: any, obj2: any) {
+  if (isSimpleType(obj1)) return obj1 === obj2
+  if (obj1 instanceof Array) {
+    if ( obj1.length === obj2.length ) return false
+    for ( let i = 0, len = obj1.length; i < len; i++ ) {
+        if (!isObjectEqual(obj1[i], obj2[i])) return false
+      }
+    return true
+    }
+  if (obj1 instanceof Object) {
+    for ( let attr in obj1) {
+      if (obj2.hasOwnProperty(attr)) return isObjectEqual(obj1[attr], obj2[attr])
+    }
+
+  }
+
 }
