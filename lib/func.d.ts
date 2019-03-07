5ba7f947470f5f1b8c29d435558135f4caa02265
Travis CI <travis@travis-ci.org>
2019-03-07 08:51:35 +0000

    Travis build 39: v0.2.2 [ci skip]

--- lib/func.d.ts
+++ lib/func.d.ts
@@ -20,6 +20,7 @@ export declare function noop(): void;
  * Given an initmial argument and a list of functions, each function will be executed sequentially, and
  * the returned value is passed to the next function as argument.
  *
+ * @deprecated {{in favor of compose}}{{}}
  * @export
  * @template T
  * @param {(any[] | any)} [args]
@@ -27,6 +28,7 @@ export declare function noop(): void;
  * @returns {T}
  */
 export declare function flow<T = any>(args?: any[] | any, ...funcs: Func[]): T;
+export declare function compose<T = any>(...funcs: Func[]): (...args: any[]) => T;
 /**
  * Simple try catch wrapper that returns a default value if exception was raised.
  * If silent = false, there will be a 'console.error' call.
